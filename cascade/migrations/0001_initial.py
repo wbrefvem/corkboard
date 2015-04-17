c0d8531e4f028c0f260defb6a93636faf0679b89
Will Refvem <wbrefvem@gmail.com>
2015-04-17 10:40:10 -0400

    Changes to migrations

--- cascade/migrations/0001_initial.py
+++ cascade/migrations/0001_initial.py
@@ -2,16 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import oauth2client.django_orm
 from django.conf import settings
 import phonenumber_field.modelfields
+import oauth2client.django_orm
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0001_initial'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -33,7 +32,7 @@ class Migration(migrations.Migration):
             name='AltDate',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('alt_date', models.DateTimeField()),
+                ('date', models.DateTimeField()),
             ],
             options={
             },
@@ -63,8 +62,10 @@ class Migration(migrations.Migration):
             name='Contact',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField()),
                 ('telephone', phonenumber_field.modelfields.PhoneNumberField(max_length=128)),
                 ('cell', phonenumber_field.modelfields.PhoneNumberField(max_length=128)),
+                ('email', models.TextField()),
                 ('address', models.ForeignKey(to='cascade.Address')),
             ],
             options={
@@ -97,7 +98,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=256)),
                 ('org_type', models.CharField(default=None, max_length=2, choices=[(b'FP', b'For Profit'), (b'NFP', b'Not for Profit')])),
-                ('contact', models.ManyToManyField(to='cascade.Contact')),
+                ('address', models.ForeignKey(to='cascade.Address')),
+                ('contact', models.OneToOneField(to='cascade.Contact')),
             ],
             options={
             },
@@ -114,9 +116,20 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='Route',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='SpecialEvent',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField()),
                 ('location', models.TextField()),
                 ('date_submitted', models.DateField()),
                 ('website', models.TextField()),
@@ -124,36 +137,36 @@ class Migration(migrations.Migration):
                 ('date', models.DateField()),
                 ('start_time', models.TimeField()),
                 ('end_time', models.TimeField()),
+                ('set_up_time', models.TimeField()),
+                ('tear_down_time', models.TimeField()),
                 ('annual_event', models.BooleanField(default=False)),
                 ('estimated_attendance', models.IntegerField()),
                 ('previous_attendance', models.IntegerField(null=True, blank=True)),
+                ('trash_removal_plan', models.TextField(null=True)),
+                ('emergency_action_plan', models.TextField(null=True)),
                 ('alcohol', models.BooleanField(default=False)),
                 ('food', models.BooleanField(default=False)),
-                ('large_tents', models.BooleanField(default=False)),
-                ('on_bus_route', models.BooleanField(default=False)),
+                ('large_tents_or_inflatables', models.BooleanField(default=False)),
+                ('bus_impact', models.BooleanField(default=False)),
                 ('amplified_music', models.BooleanField(default=False)),
-                ('pyro', models.BooleanField(default=False)),
+                ('open_flames', models.BooleanField(default=False)),
+                ('closures', models.CharField(max_length=256)),
                 ('hold_harmless_agree', models.BooleanField(default=False)),
                 ('se_notif_reqs_agree', models.BooleanField(default=False)),
                 ('legal_agree', models.BooleanField(default=False)),
                 ('app_fee_agree', models.BooleanField(default=False)),
-                ('alternative_dates', models.ManyToManyField(to='cascade.AltDate')),
+                ('status', models.CharField(default=None, max_length=2, choices=[(b'SU', b'Submitted'), (b'CO', b'Conditionally'), (b'PE', b'Permitted'), (b'PR', b'Problem')])),
+                ('alt_dates', models.ManyToManyField(to='cascade.AltDate')),
                 ('areas', models.ManyToManyField(to='cascade.Area')),
-                ('beneficiary', models.ManyToManyField(to='cascade.Beneficiary')),
-                ('event_day_contact', models.ForeignKey(to='cascade.Contact')),
+                ('beneficiaries', models.ManyToManyField(to='cascade.Beneficiary')),
                 ('event_types', models.ManyToManyField(to='cascade.EventType')),
-                ('organization', models.ManyToManyField(to='cascade.Organization')),
+                ('organizations', models.ManyToManyField(to='cascade.Organization')),
                 ('participant_types', models.ManyToManyField(to='cascade.ParticipantType')),
                 ('previous_events', models.ManyToManyField(related_name='previous_events_rel_+', to='cascade.SpecialEvent')),
+                ('route', models.OneToOneField(to='cascade.Route')),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='contact',
-            name='user',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
-            preserve_default=True,
         ),
     ]
